e05db104aa9416ef35023dfde7c1179821dfddde
Amit Chinara <amitchinara@gmail.com>
2025-01-19 10:44:00 +0530

    First Commit

---
+++ api/src/main/java/com/project/userManagementSystem/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.project.userManagementSystem.controller;
+
+import com.project.userManagementSystem.model.User;
+import com.project.userManagementSystem.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
---
+++ api/src/main/java/com/project/userManagementSystem/model/User.java
@@ -0,0 +1,15 @@
+package com.project.userManagementSystem.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Getters and Setters
+}
